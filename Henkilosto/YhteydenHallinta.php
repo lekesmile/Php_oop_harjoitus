0d7c3e5de1bd098b0204930c45811d304bcde873
leke <lekesmile1@gmail.com>
2019-10-09 09:46:58 +0300

    updated

--- Henkilosto/YhteydenHallinta.php
+++ Henkilosto/YhteydenHallinta.php
@@ -69,7 +69,7 @@ class YhteydenHallinta{
 
     public function suoritaPaivitysLause($sqlLause, $parametritaulukko = Array()){
         $this->avaaYhteys();
-        print_r ($parametritaulukko);
+        // print_r ($parametritaulukko);
 
         try {
             // Valmistellaa SQL-Lause
